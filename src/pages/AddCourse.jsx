ef073d9b0a587a7d02d7213d8b1796dd6fee0015
arwaalhassan <142218360+arwaalhassan@users.noreply.github.com>
2026-02-01 12:35:54 +0300

    Update AddCourse.jsx

--- src/pages/AddCourse.jsx
+++ src/pages/AddCourse.jsx
@@ -134,7 +134,7 @@ const AddCourse = () => {
                 </select>
               </div>
               <div>
-                <label className="block text-sm font-bold mb-1 mr-1">السعر (SP) *</label>
+                <label className="block text-sm font-bold mb-1 mr-1">السعر ($) *</label>
                 <input 
                   type="number" 
                   className="w-full p-3 border rounded-xl outline-none"
